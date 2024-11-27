a012bca2e780c913ae3dbae248dcc5e7fd50f8b8
PolitSolelh <132207294+PolitSolelh@users.noreply.github.com>
2024-11-27 14:20:07 +0100

    added buttons to generate a number
    + a reset option

--- script.js
+++ script.js
@@ -12,21 +12,35 @@ function choixDifficulte(max) {
     {
         const minCeiled = Math.ceil(1);
         const maxFloored = Math.floor(50);
-        return justePrix = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+        document.getElementById("secret").innerHTML = justePrix = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+        document.getElementById("difficulty").innerHTML = "Vous avez choisi la difficulté Facile";
     }
     else if(max === 100) 
     {
         const minCeiled = Math.ceil(1);
         const maxFloored = Math.floor(100);
-        return justePrix = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+        document.getElementById("secret").innerHTML = justePrix = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+        document.getElementById("difficulty").innerHTML = "Vous avez choisi la difficulté Moyenne"
     }
-    else(max === 1000) 
+    else if(max === 1000) 
     {
         const minCeiled = Math.ceil(1);
         const maxFloored = Math.floor(1000);
-        return justePrix = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+        document.getElementById("secret").innerHTML = justePrix = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+        document.getElementById("difficulty").innerHTML = "Vous avez choisi la difficulté Difficile"
     }
+
+    document.getElementById("easy").disabled = true;
+    document.getElementById("normal").disabled = true;
+    document.getElementById("hard").disabled = true;
 }
 
-choixDifficulte(1000)
-console.log(justePrix);
+function reset() {
+    document.getElementById("easy").disabled = false;
+    document.getElementById("normal").disabled = false;
+    document.getElementById("hard").disabled = false;
+    document.getElementById("difficulty").innerHTML = "Sélectionnez une difficulté"
+    justePrix = 0;
+}
+
+console.log(max);
