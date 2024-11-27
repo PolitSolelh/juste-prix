55812410af4dd5af2a718754cc385d55f25a3a89
PolitSolelh <132207294+PolitSolelh@users.noreply.github.com>
2024-11-27 16:05:39 +0100

    cleaned code

--- script.js
+++ script.js
@@ -6,46 +6,47 @@ function choixDifficulte(max) {
     {
         const minCeiled = Math.ceil(1);
         const maxFloored = Math.floor(50);
-        document.getElementById("secret").innerHTML = justePrix = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+        justePrix = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
         document.getElementById("difficulty").innerHTML = "Vous avez choisi la difficulté Facile";
     }
     else if(max === 100) 
     {
         const minCeiled = Math.ceil(1);
         const maxFloored = Math.floor(100);
-        document.getElementById("secret").innerHTML = justePrix = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+        justePrix = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
         document.getElementById("difficulty").innerHTML = "Vous avez choisi la difficulté Moyenne"
     }
     else if(max === 1000) 
     {
         const minCeiled = Math.ceil(1);
         const maxFloored = Math.floor(1000);
-        document.getElementById("secret").innerHTML = justePrix = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+        justePrix = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
         document.getElementById("difficulty").innerHTML = "Vous avez choisi la difficulté Difficile"
     }
 
-    document.getElementById("easy").disabled = true;
-    document.getElementById("normal").disabled = true;
-    document.getElementById("hard").disabled = true;
-    document.getElementById("guess").disabled = false;
-    document.getElementById("send").disabled = false;
+    document.getElementById("easy").style.display = "none";
+    document.getElementById("normal").style.display = "none";
+    document.getElementById("hard").style.display = "none";
+    document.getElementById("guess").style.display = "flex";
+    document.getElementById("send").style.display = "flex";
 }
 
 function reset() {
-    document.getElementById("easy").disabled = false;
-    document.getElementById("normal").disabled = false;
-    document.getElementById("hard").disabled = false;
-    document.getElementById("guess").disabled = true;
-    document.getElementById("send").disabled = true;
+    document.getElementById("easy").style.display = "flex";
+    document.getElementById("normal").style.display = "flex";
+    document.getElementById("hard").style.display = "flex";
+    document.getElementById("guess").style.display = "none";
+    document.getElementById("send").style.display = "none";
     document.getElementById("difficulty").innerHTML = "Sélectionnez une difficulté";
+    document.getElementById("tentatives").innerHTML = "";
+    document.getElementById("prix").innerHTML = "";
+    document.getElementById("guess").value = "";
     tries = 0;
     justePrix = 0;
 }
 
 function humanGuess() {
     let guess = document.getElementById("guess").value;
-    console.log(guess);
-    console.log(justePrix);
 
     if(tries < 14) {
         if(guess > justePrix) 
@@ -66,8 +67,8 @@ function humanGuess() {
         tries++;
         document.getElementById("tentatives").innerHTML = "";
         document.getElementById("prix").innerHTML = `Vous avez gagné en ${tries} essais ! Le juste prix est : ${justePrix}`;
-        document.getElementById("guess").disabled = true;
-        document.getElementById("send").disabled = true;
+        document.getElementById("guess").style.display = "none";
+        document.getElementById("send").style.display = "none";
 
         }
     }
