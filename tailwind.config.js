b0865364fa7a58670f91b7fab41d91436d9260fc
PolitSolelh <132207294+PolitSolelh@users.noreply.github.com>
2024-12-10 12:08:37 +0100

    changed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
       fontFamily: {
         'coolvetica': ['Coolvetica']
     },
+      colors: {
+        'black': '#423736',
+        'purple': '#987185',
+        'salmon': '#d6aa9f',
+        'yellowish': '#e9d5b7',
+        'white': '#f4e2d1',
+      },
+      width: {
+        '128' : '32rem',
+      }
   },
   },
   plugins: [],
